0e1a4502950d62cf940538eae0b697d155128195
Ellis Trickett <ellis.trickett@gmail.com>
2020-11-11 17:55:14 +0000

    Tile and Task Models added with foreign key

--- tiles/tilesapi/admin.py
+++ tiles/tilesapi/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Tile
+from .models import Task
 
 # Register your models here.
+admin.site.register(Tile)
+admin.site.register(Task)
+
--- tiles/tilesapi/models.py
+++ tiles/tilesapi/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+STATUS_CHOICES = [
+  ('live', 'Live'),
+  ('pending', 'Pending'),
+  ('archived', 'Archived')
+]
+
+class Tile(models.Model):
+  title = models.CharField(max_length=50)
+  launch_date = models.CharField(max_length=50)
+  status = models.CharField(choices=STATUS_CHOICES, max_length=12, db_index=True)
+
+  def __str__(self):
+    return self.title
+
+class Task(models.Model):
+  title = models.CharField(max_length=50)
+  order_field = models.CharField(max_length=50)
+  description = models.CharField(max_length=200)
+  type = models.CharField(max_length=200)
+  tile = models.ForeignKey(Tile, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.title
