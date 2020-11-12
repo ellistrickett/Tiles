b7fae4256611bb7d0465eb12a000c84c379be48d
Ellis Trickett <ellis.trickett@gmail.com>
2020-11-12 17:47:21 +0000

    added views for task and tile

--- tiles/tilesapi/views.py
+++ tiles/tilesapi/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import TileSerializer
+from .models import Tile
+
+from .serializers import TaskSerializer
+from .models import Task
+
+class TileViewSet(viewsets.ModelViewSet):
+    queryset = Tile.objects.all().order_by('title')
+    serializer_class = TileSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all().order_by('title')
+    serializer_class = TaskSerializer
